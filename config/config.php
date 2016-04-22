33efa3ba092c8da83fe2ce2916464709ff3e0c6d
OttoOsterman <ottosterman@gmail.com>
2016-04-22 18:17:35 +0100

    Merge branch 'master' of C:\Users\Otto\PhpstormProjects\PocketPantry with conflicts.

---
+++ config/config.php
@@ -0,0 +1,9 @@
+<?php
+/************************************************
+ ATTENTION: Fill in these values! Make sure
+the redirect URI is to this page, e.g:
+http://localhost:8080/user-example.php
+************************************************/
+$client_id = ' 568823689660-386ik3kjs8gbu26p6l9i9eveoa5fk0c2.apps.googleusercontent.com ';
+$client_secret = ' ToU8HWKKRyp6kr2s-gCaNx8U ';
+$redirect_uri = 'postmessage';
